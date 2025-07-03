3e5c53b4f0037a7c539a4bc8d69095e385911016
renecastrotm <rene@terramundi.com.br>
2025-07-03 19:20:23 -0300

    server.js

--- server.js
+++ server.js
@@ -10,7 +10,10 @@ app.get('/cotacao', async (req, res) => {
   try {
     const response = await axios.get('https://br.investing.com/currencies/usd-brl', {
       headers: {
-        'User-Agent': 'Mozilla/5.0'
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
+        'Accept-Language': 'pt-BR,pt;q=0.9,en-US;q=0.8,en;q=0.7',
+        'Referer': 'https://www.google.com/',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8'
       }
     });
 
